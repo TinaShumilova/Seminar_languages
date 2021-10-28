d4d97cfcc47a71b4e10d8745f5c12871a0c7650e
Tina Shumilova <shutiva@gmail.com>
2021-10-28 14:38:01 +0300

    add task 2

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,10 +1,31 @@
 ﻿//Задача по смене мест
-int a = 1;
-int b = 3;
+//int a = 1;
+//int b = 3;
 //Console.WriteLine("a = " + a + " b = " + b);
 //Console.WriteLine("a = {0} b = {1}", a, b);
-Console.WriteLine($"a = {a} b = {b}");
-int temp = a;
-a = b;
-b = temp;
-Console.WriteLine($"a = {a} b = {b}");
+//Сonsole.WriteLine($"a = {a} b = {b}");
+//int temp = a;
+//a = b;
+//b = temp;
+//Console.WriteLine($"a = {a} b = {b}");
+//Console.WriteLine($"a = {a} b = {b}");
+//a = a+b;
+//b = a-b;
+//a = a-b;
+//Console.WriteLine($"a = {a} b = {b}");
+
+//Задача на поиск максимального числа
+int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+Console.WriteLine(a);
+Console.WriteLine(b);
+if(a>b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
