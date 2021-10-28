3de1ec92fdd4c48db8fdf43fb0348ac5fb002421
Tina Shumilova <shutiva@gmail.com>
2021-10-28 14:11:09 +0300

    task 1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача по смене мест
+int a = 1;
+int b = 3;
+//Console.WriteLine("a = " + a + " b = " + b);
+//Console.WriteLine("a = {0} b = {1}", a, b);
+Console.WriteLine($"a = {a} b = {b}");
+int temp = a;
+a = b;
+b = temp;
+Console.WriteLine($"a = {a} b = {b}");
