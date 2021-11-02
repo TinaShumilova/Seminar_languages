b6439990b2fb129a8dd23fc45844a4973e8fc3d0
Tina Shumilova <shutiva@gmail.com>
2021-11-02 13:17:59 +0200

    task2

--- Praktika_2/Program.cs
+++ Praktika_2/Program.cs
@@ -5,7 +5,146 @@
 - элементы, больше 8
 - знакочередования*/
 
-int CreateArray(int[] arreyLenth, int minValue, maxValue)
+/*void CreateArray(int[] collection, int minValue, int maxValue)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(minValue+1, maxValue+1);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int possition = 0;
+    while(possition < count)
+    {
+        Console.Write($"{array[possition]} ");
+        possition++;
+    }
+    Console.WriteLine();
+}
+*/
+/*int[] Increase(int[] col)
+{
+    int count = 1;
+    int [] arr = new int[count];
+    int i = 0;
+    arr[i] = col[0];
+    int length = col.Length;
+    int index = 1;
+    int temp = col[0];
+    while(index < 10)
+    {
+        if(col[index] > arr[i])
+        {
+            temp = col[index];
+            count++;
+            i++;
+            arr[i] = temp;
+            index++;
+        }
+    }
+    return arr;
+}
+
+void PrintArrayB(int[] B)
+{
+    int count = B.Length;
+    int possition = 1;
+    Console.Write($"{B[0]} ");
+    while(possition < count)
+    {
+        Console.Write($"{B[possition]} ");
+        possition++;
+    }
+    Console.WriteLine();
+}
+*/
+
+
+
+/*[] ArrayA = new int[10];
+CreateArray(ArrayA, 1, 100);
+PrintArray(ArrayA);
+int[] ArrayB = Increase(ArrayA);
+PrintArray(ArrayB);*/
+
+Console.WriteLine();
+Console.WriteLine("Создаём числовой массив A заполненный числами из отрезка [-10, 10].");
+Console.WriteLine();
+Console.Write("Массив А : ");
+int length = 10;
+int[] ArrayA = new int[length];
+int index = 0;
+while (index < length)
+{
+    ArrayA[index] = new Random().Next(-10,11);
+    Console.Write($"{ArrayA[index]} ");
+    index++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("Создаём на его основе масив B, отбрасывая те элементы, которые");
+Console.WriteLine();
+Console.WriteLine("1. нарушают порядок возрастания,");
+Console.WriteLine();
+
+index = 1;
+int temp = ArrayA[0];
+Console.Write($"Массив В.1 : {temp} ");
+while(index < length)
+{
+    if(ArrayA[index] > temp)
+    {
+        Console.Write($"{ArrayA[index]} ");
+        temp = ArrayA[index];
+    }
+    index++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("2. больше 8,");
+Console.WriteLine();
+
+index = 0;
+Console.Write("Массив В.2 : ");
+while(index < length)
+{
+    if(ArrayA[index] < 8)
+    {
+        Console.Write($"{ArrayA[index]} ");
+    }
+    index++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("3. нарушают порядок знакочередования, ");
+Console.WriteLine();
+
+/*int index = 1;
+int currentElement = ArrayA[0];
+Console.Write($"{currentElement} ");
+while(index < 10)
+{
+    if(ArrayA[index] > currentElement)
+    {
+        Console.Write($"{ArrayA[index]} ");
+        currentElement = ArrayA[index];
+    }
+    index++;
+}
+Console.WriteLine();
+
+
+
+/*int CreateArray(int[] arreyLenth, int minValue, int maxValue)
 {
     int numArray = new int[arreyLenth];
     int index = 0;
@@ -29,4 +168,4 @@ int RemoveInt(int[] A)
 
 
 int[] A = CreateArray(5, 1, 15);
-int[] B = RemoveInt()
+int[] B = RemoveInt()*/
