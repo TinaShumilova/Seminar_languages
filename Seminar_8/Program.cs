fb280e3cc918eda2e9f62e7e2378a83b6446785f
Tina Shumilova <shutiva@gmail.com>
2021-11-23 20:08:26 +0200

    done tasks No 45

--- Seminar_8/Program.cs
+++ Seminar_8/Program.cs
@@ -50,6 +50,12 @@ int count(int[] array)
     }
     return count;
 }
+(double, double) FindXY(double k1, double b1, double k2, double b2)
+{
+    double X = (b2 - b1) / (k1 - k2);
+    double Y = k2 * X + b2;
+    return (X, Y);
+}
 */
 // Почувствуй себя лидом
 // 41. Выяснить являются ли три числа сторонами треугольника 
@@ -103,26 +109,39 @@ Console.WriteLine(ConvToDouble(num));
 // y = k1 * x + b1, 
 // y = k2 * x + b2, 
 // b1 k1 и b2 и k2 заданы
+/*
 double b1 = -1;
 double k1 = 2;
 double b2 = 1;
 double k2 = -3;
 
-(double, double) FindXY(double k1, double b1, double k2, double b2)
-{
-    double X = (b2 - b1) / (k1 - k2);
-    double Y = k2 * X + b2;
-    return (X, Y);
-}
 Console.WriteLine(FindXY(k1, b1, k2, b2));
-
-/*
-double X = 2 / 5;
-double Y = 1 / 3;
-Console.WriteLine($"{X}");
-Console.WriteLine(Y);
 */
+
 // 45. Показать числа Фибоначчи
+//a, b, c = a+b
+int[] Fibbonachi(int a, int b, int lenght)
+{
+    int[] array = new int[lenght];
+    array[0] = a;
+    array[1] = b;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array;
+}
+string PrintArray(int[] array)
+{
+    string text = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        text = text + $"{array[i]} ";
+    }
+    return text;
+}
+int[] Fibo = Fibbonachi(0, 1, 10);
+Console.WriteLine(PrintArray(Fibo));
 // 46. Написать программу масштабирования фигуры
 /*===============================================
 Тут для тех кто далеко улетел, чтобы задавались вершины фигуры списком (одной строкой)
