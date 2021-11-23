527861986bb86200132aee09d46bc626c97aa8d2
Tina Shumilova <shutiva@gmail.com>
2021-11-23 18:16:39 +0200

    done tasks No 41 -42 and half 43

--- Seminar_8/Program.cs
+++ Seminar_8/Program.cs
@@ -1,7 +1,93 @@
-﻿// Почувствуй себя лидом
+﻿/*bool Tripl(int a, int b, int c)
+{
+    return ((a + b > c) && (a + c > b) && (c + b > a));
+}
+int[] array(string tx)
+{
+    int lenght = 0;
+    tx = tx + " ";
+    for (int i = 0; i < tx.Length; i++)
+    {
+        if (tx[i] == ' ') lenght++;
+    }
+    return new int[lenght];
+}
+int[] FillArFromTex(int[] arr, string tx)
+{
+    tx += " ";
+    string temp = String.Empty;
+    int i = 0;
+    for (int j = 0; j < tx.Length; j++)
+    {
+        if (tx[j] != ' ')
+        {
+            temp = temp + tx[j];
+        }
+        else
+        {
+            arr[i] = Convert.ToInt32(temp);
+            temp = String.Empty;
+            i++;
+        }
+    }
+    return arr;
+}
+string PrintArray(int[] array)
+{
+    string text = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        text = text + $"{array[i]} ";
+    }
+    return text;
+}
+int count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count++;
+    }
+    return count;
+}
+*/
+// Почувствуй себя лидом
 // 41. Выяснить являются ли три числа сторонами треугольника 
+/*
+Console.Clear();
+int AB = 8;
+int BC = 6;
+int CA = 10;
+bool answer = Tripl(AB, BC, CA);
+Console.WriteLine(answer);
+*/
 // 42. Определить сколько чисел больше 0 введено с клавиатуры
+/*
+string text = Console.ReadLine();
+int[] ArrayT = array(text);
+FillArFromTex(ArrayT, text);
+Console.WriteLine(count(ArrayT));
+*/
 // 43. Написать программу преобразования десятичного числа в двоичное
+int num = 44;
+string ConvToDouble(int number)
+{
+    string temp = String.Empty;
+    while(number/2 != 0)
+    {
+        if(number%2==0)
+        {
+            temp = "0" + temp;
+        }
+        else
+        {
+            temp = "1" + temp;
+        }
+        number = number/2;
+    }
+    return temp;
+}
+Console.WriteLine(ConvToDouble(num));
 // 44. Найти точку пересечения двух прямых заданных уравнением y=kx+b, а1 k1 и а2 и k2 заданы
 // 45. Показать числа Фибоначчи
 // 46. Написать программу масштабирования фигуры
